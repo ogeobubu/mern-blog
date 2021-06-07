8818e855d92f98c2c627243651cd1a0f1fe89423
Oge Obubu <ogeobubu@gmail.com>
2021-06-07 04:36:59 +0100

    added deployed routes

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/category", categoryRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
